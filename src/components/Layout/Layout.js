9d77ad0c96a53bad8dcd849c02c551de6cac0c9f
Alfonso Achiaga <alfonso.achiaga@gmail.com>
2019-04-02 18:01:26 +0900

    Create Burger Builder and style Layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Aux from "../../hoc/Aux";
+import classes from "./Layout.module.css"
 // We need to creat an Aux or array or in a div or something bc we can not use jsx elements in js
 const layout = (props) => (
     <Aux>
         <div>Toolbar, SideDrawer, Backdrop</div>
-        <main>
+        <main className={classes.Content}>
             {props.children}
         </main>
     </Aux>
