763b980878c2e8b5cb0aa0953071e7efd989e756
Alfonso Achiaga <alfonso.achiaga@gmail.com>
2019-04-03 23:57:20 +0900

    Burger Ingridient Dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,18 @@ import classes from "./Burger.module.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
  
 const burger = (props) => {
+    // Only pass the string no the numbers in Object.key
+    // Keys return an array
+    const transformedIngredents = Object.keys(props.ingridients).map(igKey => {
+        return [...Array(props.ingredients[igKey])].map( (_, i) => {
+            return <BurgerIngredient key={igKey + i} type={igKey} />;
+        });
+    });
+
     return(
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {transformedIngredents}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,23 @@ import Aux from "../../hoc/Aux";
 import Burger from "../../components/Burger/Burger"
 
 class BurgerBuilder extends Component {
+    // contructor(props) {
+    //     super(props);
+    //     this.state = {...}
+    // }
+    state = {
+        ingridients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat: 2
+        }
+    }
+    
     render() {
         return(
             <Aux>
-                <Burger />
+                <Burger ingridients={this.state.ingridients}/>
                 <div>Build Controls</div>
             </Aux>
         );
