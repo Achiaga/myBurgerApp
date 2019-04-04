fec13fb2f3fa96db8440956df5e8710b7b95ce58
Alfonso Achiaga <alfonso.achiaga@gmail.com>
2019-04-04 14:31:36 +0900

    Add build control (buttons) and style them

---
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import classes from "./BuildControl.module.css"
+ 
+const buildControl = (props) => (
+    <div className={classes.BuildControl}>
+        {/* name of ingredient I want to add */}
+        <div className={classes.Label}>{props.label}</div>
+        <button className={classes.Less}>Less</button>
+        <button className={classes.More}>More</button>
+    </div>
+);
+ 
+ 
+export default buildControl;
