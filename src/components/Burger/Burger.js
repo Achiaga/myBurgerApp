d9178bb720dba21b53460e2b591a20df2cd04df7
Alfonso Achiaga <alfonso.achiaga@gmail.com>
2019-04-04 13:24:41 +0900

    Add <p> when ingredients is empty

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,12 +5,24 @@ import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 const burger = (props) => {
     // Only pass the string no the numbers in Object.key
     // Keys return an array
+    // Transform object into Array:
     let transformedIngredients = Object.keys(props.ingredients).map(igKey => {
         return [...Array(props.ingredients[igKey])].map( (_, i) => {
             return <BurgerIngredient key={igKey + i} type={igKey} />;
         });
-    });
-
+    })
+    // transformed array into something else
+    // takes finction as argument
+    // this function takes two arguments (previous value, current value)
+    // returning somwthing --> loop through all elements and add them to the initial value step by step
+    // concat (el) take the given element we are looping and add it to the array
+    .reduce((arr, el) => {
+        return arr.concat(el)
+    }, []);
+    if (transformedIngredients.length === 0){
+        transformedIngredients = <p>Please, Start Adding Ingredients!</p>
+    }
+    console.log(transformedIngredients)
     return(
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
