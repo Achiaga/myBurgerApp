5ee17b57b96ca0b73389e2d4bd84604dfc97588e
Alfonso Achiaga <alfonso.achiaga@gmail.com>
2019-04-06 18:50:32 +0900

    Add Navigation with the logo and toolbar

--- src/components/Burger/OrderSummary/OrderSummery.js
+++ src/components/Burger/OrderSummary/OrderSummery.js
@@ -20,6 +20,7 @@ const orderSummery = (props) => {
                 {ingredientSummery}
             </ul>
             <p>Continue To Chekout?</p>
+            <p><strong>Total Price: ${props.price.toFixed(2)} </strong></p>
             <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
             <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
         </Aux>
