4fb49db96ee47a768b212bab377018d51f2ce728
Alfonso Achiaga <alfonso.achiaga@gmail.com>
2019-04-07 17:25:04 +0900

    Add SideDrawer and Toggle Button

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from "../../hoc/Aux";
 import classes from "./Layout.module.css"
 import Toolbar from "../Navigation/Toolbar/Toolbar"
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer"
 // We need to creat an Aux or array or in a div or something bc we can not use jsx elements in js
-const layout = (props) => (
-    <Aux>
-        <Toolbar />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
+class Layout extends Component {
+    state = {
+        showSideDrawer: false,
+    }
 
-export default layout;
+    SideDrawerCloseHandler = () => {
+        this.setState({showSideDrawer: false})
+    }
+
+    SideDrawerToggleHandler = () => {
+        this.setState((prevState) => {
+            return {showSideDrawer: !prevState.showSideDrawer}
+        })
+    }
+
+    render() {
+        return(
+            <Aux>
+                <Toolbar toggleMenu={this.SideDrawerToggleHandler} />
+                <SideDrawer 
+                open={this.state.showSideDrawer}
+                closed={this.SideDrawerCloseHandler}
+                 />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>             
+        )  
+    }
+}
+
+export default Layout;
