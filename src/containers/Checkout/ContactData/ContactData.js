093d7a9a25b26d72d43e7749a07f30d27a9e2bb2
Achiaga <alfonso.achiaga@gmail.com>
2019-05-07 13:25:22 +0900

    Use Utility Function in all the App

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
+import { connect } from "react-redux";
+
 import Button from "../../../components/UI/Button/Button"
 import classes from "./ContactData.module.css"
 import axios from "../../../axios-orders"
 import Spinner from "../../../components/UI/Spinner/Spinner"
 import Input from "../../../components/UI/Input/Input"
-import { connect } from "react-redux";
 import withErrorHandler from "../../../hoc/withErrorHandler/withErrorHandler"
 import * as actions from "../../../store/actions/index"
+import { updateObject } from "../../../shared/utility"
  
 class ContactData extends Component {
     state = {
@@ -152,18 +154,17 @@ class ContactData extends Component {
     }
 
     inputChangedHandler = (event, inputIdentifier) => {
-        const updatedOrderForm = {
-            ...this.state.orderForm
-        }
 
-        const updatedFormElement = {
-            ...updatedOrderForm[inputIdentifier]
-        }
+        const updatedFormElement = updateObject(this.state.orderForm[inputIdentifier],  {
+            value: event.target.value,
+            valid: this.checkValidity(event.target.value, this.state.orderForm[inputIdentifier].validation),
+            touched: true
+        })
 
-        updatedFormElement.value = event.target.value;
-        updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation)
-        updatedFormElement.touched = true;
-        updatedOrderForm[inputIdentifier] = updatedFormElement;
+        const updatedOrderForm = updateObject(this.state.orderForm, {
+            [inputIdentifier]: updatedFormElement
+        }) 
+
         let formIsValid = true;
         for (let inputIdentifier in updatedOrderForm){
             formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
